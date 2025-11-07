11f37666fda345c85589b04e4cf8313d465a6656
eric-burel <eb@lbke.fr>
2025-11-07 19:01:42 +0100

    simplify chroma integration

--- src/react_agent/langchain_doc_retriever.py
+++ src/react_agent/langchain_doc_retriever.py
@@ -1,49 +1,29 @@
 # Initialize an ephemercal in-memory Chroma vector store
 # It will disappear when we stop the agent
 # We import it in graph.py to feed the agent with documents
-from langchain_chroma import Chroma
-from chromadb.utils import embedding_functions
-from langchain_core.documents import Document
-
-# Chroma provides a default embedding function
-# But it is not used in LangGraph => we write a quick dummy wrapper to reuse this function
-# @see https://docs.trychroma.com/docs/embeddings/embedding-functions#custom-embedding-functions
-# TODO: ideally, the Chroma vector store should use this function as a default
-# TODO: we could add explicit errors for the async functions
-
-
-class ChromaEmbeddings:
-    def __init__(self):
-        self.embd = embedding_functions.DefaultEmbeddingFunction()
-
-    def embed_query(self, query):
-        return self.embd([query])[0]
-
-    def embed_documents(self, docs):
-        return self.embd(docs)
-
-
-embeddings = ChromaEmbeddings()
+import chromadb
+chroma_client = chromadb.Client()
 documents = [
-    Document("LangChain invokes LLMs"),
-    Document("LangGraph runs agents")
+    {"id": "1", "content": "LangChain invokes LLMs"},
+    {"id": "2", "content": "LangGraph runs agents"},
 ]
-vector_store = Chroma(
-    collection_name="example_collection",
-    embedding_function=embeddings,
+collection = chroma_client.create_collection(name="langchain_documentation")
+collection.upsert(
+    # Génération d'identifiants uniques pour chaque document
+    ids=[d["id"] for d in documents],
+    documents=[d["content"] for d in documents],
 )
-vector_store.add_documents(documents)
-
-langchain_doc_retriever = vector_store.as_retriever(search_kwargs={
-    "k": 1
-})
-
 
 # Test with
 # python ./src/langchain_doc_retriever.py
 if __name__ == "__main__":
-    retriever = langchain_doc_retriever
-    res_langchain = retriever.invoke("What is LangChain?")[0]
-    res_langgraph = retriever.invoke("What is LangGraph?")[0]
-    print(f"Expects LangChain document, got {res_langchain}")
-    print(f"Expects LangGraph document, got {res_langgraph}")
+    collection = chroma_client.get_collection(name="langchain_documentation")
+    queries = ["What is LangChain?", "What is LangGraph?"]
+    for query in queries:
+        results = collection.query(
+            query_texts=[query],
+            n_results=1
+        )
+        print(f"Query: {query}")
+        for doc in results['documents'][0]:
+            print(f" - Retrieved document: {doc}")
--- src/react_agent/tools.py
+++ src/react_agent/tools.py
@@ -15,16 +15,8 @@ from langgraph.runtime import get_runtime
 
 from react_agent.context import Context
 
-
 # Test version with 2 dummy docs about LangChain and LangGraph
-from react_agent.langchain_doc_retriever import langchain_doc_retriever
-# Version that actually loads LangChain documentation
-from react_agent.langsmith_doc_retriever_complete import get_langsmith_doc_retriever
-
-# Uncomment to ingest actual LangChain doc,
-# it will take some time to run on the first iteration (in minutes)
-# result = langsmith_doc_retriever.invoke(query)
-langsmith_doc_retriever = get_langsmith_doc_retriever()
+from react_agent.langchain_doc_retriever import collection as langchain_documents_collection
 
 
 async def search(query: str) -> Optional[list[dict[str, Any]]]:
@@ -53,26 +45,15 @@ def search_langchain(
     for answering questions about LangChain and LangGraph.
     """
     # result = retriever.invoke(query)
-    result = langchain_doc_retriever.invoke(query)
-    return cast(list[dict[str, Any]], result)
+    results = langchain_documents_collection.query(
+        query_texts=[query],
+        n_results=1)
+    # Results is a list of lists (many documents for many entry queries)
+    # Here we have only one query, so we return the first list
+    return results["documents"][0]
 
 
 print(search_langchain.description)
 
 
-def search_langsmith(
-    query: str
-) -> Optional[list[dict[str, Any]]]:
-    """Search in LangSmith official documentation.
-
-    This tool should be used in priority over a generic web search. 
-
-    It is is designed to provide comprehensive, accurate, and trusted results. It's particularly useful
-    for answering questions about LangSmith
-    """
-    # result = retriever.invoke(query)
-    result = langsmith_doc_retriever.invoke(query)
-    return cast(list[dict[str, Any]], result)
-
-
-TOOLS: List[Callable[..., Any]] = [search, search_langsmith, search_langchain]
+TOOLS: List[Callable[..., Any]] = [search, search_langchain]
